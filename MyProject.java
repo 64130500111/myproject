d965e9e2ec93feb9d3d8b1c054110e1648716e64
Supakorn Chat-anothai <supakorn.chat@kmutt.ac.th>
2022-03-23 12:02:33 +0700

    new file

--- MyProject.java
+++ MyProject.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package sit.bscit.myproject;
 
-/**
- *
- * @author Administrator
- */
 public class MyProject {
     
 }
