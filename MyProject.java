3fdb8ec075190248cb28b9a2432d8083806df1b6
Supakorn Chat-anothai <supakorn.chat@kmutt.ac.th>
2022-03-30 09:43:29 +0700

    Hello 1 2 3

--- MyProject.java
+++ MyProject.java
@@ -2,7 +2,7 @@ package sit.bscit.myproject;
 
 public class MyProject {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! 1 2 3");
         
     }
 }
