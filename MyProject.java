34f5e4c8bb32a802ce81c42892f3c217d650db12
Supakorn Chat-anothai <supakorn.chat@kmutt.ac.th>
2022-03-30 10:29:35 +0700

    this is present

--- MyProject.java
+++ MyProject.java
@@ -3,8 +3,7 @@ package sit.bscit.myproject;
 public class MyProject {
     public static void main(String[] args) {
         System.out.println("Hello World! 1 2 3");
-        System.out.println("INT103 Advance Programing");
-        System.out.println("before revert");
+        System.out.println("revert success this is present");
         
     }
 }
