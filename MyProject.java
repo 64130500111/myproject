e8b9ecbf1709a55fe293f7e8addef2b0362c2f6a
Supakorn Chat-anothai <supakorn.chat@kmutt.ac.th>
2022-03-30 09:58:30 +0700

    merge + add sout

--- MyProject.java
+++ MyProject.java
@@ -3,6 +3,7 @@ package sit.bscit.myproject;
 public class MyProject {
     public static void main(String[] args) {
         System.out.println("Hello World! 1 2 3");
+        System.out.println("INT103 Advance Programing");
         
     }
 }
