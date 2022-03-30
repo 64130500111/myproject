c7ee669960a45b36cbecf472fd8420f7b8c1537e
Supakorn Chat-anothai <supakorn.chat@kmutt.ac.th>
2022-03-30 10:22:21 +0700

    before revert

--- MyProject.java
+++ MyProject.java
@@ -6,6 +6,7 @@ public class MyProject {
     public static void main(String[] args) {
         System.out.println("Hello World! 1 2 3");
         System.out.println("INT103 Advance Programing");
+        System.out.println("before revert");
         
     }
 }
